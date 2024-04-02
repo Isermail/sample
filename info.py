55aa2038d903d3dce09310cc747bd20bec38870b
Isermail <154401000+Isermail@users.noreply.github.com>
2024-04-02 16:04:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ support_chat_id = environ.get('SUPPORT_CHAT_ID', '') # support group id ( make s
 reqst_channel = environ.get('REQST_CHANNEL_ID', '') # request channel id ( make sure bot is admin )
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
-NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
+NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cugyfu:zTB0uhJp5nkSG1CZ@cluster0.6xvh1jw.mongodb.net/?retryWrites=true&w=majority")
